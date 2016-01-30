4e95968762bad8543ca95778f79908727e8f674c
thrusanova <thrusanova@gmail.com>
2016-01-30 13:46:13 +0200

    Java-Basics

---
+++ Intro-Java-Homework/src/com/company/AssignVariables.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by tanya on 12/15/2015.
+ */
+public class AssignVariables {
+    public  static  void  main(String []args) {
+        byte a=127;
+        short b=32767;
+        boolean c=false;
+        int d=2000000000;
+        long e=919827112351L;
+        double f=0.1234567891011;
+        float h=0.5f;
+        String k="Palto Alto, CA";
+        char ch='c';
+
+    }
+
+}
